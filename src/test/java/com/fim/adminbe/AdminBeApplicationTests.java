c76f9e2a1aaed812e8fa5a464bab52c1fe383da4
TJOSERAFAEL <ticjoserafael@gmail.com>
2019-12-31 12:59:08 +0100

    Initial commit

---
+++ src/test/java/com/fim/adminbe/AdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fim.adminbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
